76fabd41df20cc0bfb4c2b2523d617e18c470f6b
NainaMatthieu <89472863+NainaMatthieu@users.noreply.github.com>
2023-08-01 14:21:35 +0300

    Création serveur https

--- app.js
+++ app.js
@@ -1,25 +1,29 @@
 const express = require('express');
-const app = express();
-
-const cors  = require('cors');
+const https = require('https');
+const fs = require('fs');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 require('./util/DBConnexion');
 
-// insertion data
-// const insertTS_Cat = require('./util/insertionData');
-// insertTS_Cat()
-//   .then(insertedId => {
-//     console.log('Id inséré:', insertedId);
-//   })
-//   .catch(err => {
-//     console.error('Erreur lors de l\'insertion du document:', err);
-//   });
-app.use(bodyParser.json());
+const app = express();
+const certFilePath = './certificat/certificate.pem';
+const keyFilePath = './certificat/privatekey.pem';
+// Lecture du certificat et de la clé privée
+const cert = fs.readFileSync(certFilePath);
+const key = fs.readFileSync(keyFilePath);
+
+// Options serveur HTTPS
+const options = {
+  cert: cert,
+  key: key
+};
+const server = https.createServer(options, app);
 app.use(cors());
+app.use(bodyParser.json());
 
 const TouristSpotRoute = require('./router/TouristSpotsRoute');
-app.use('/touristspots',TouristSpotRoute);
+app.use('/touristspots', TouristSpotRoute);
 
 const CategorieRoute = require('./router/CategorieRoute');
-app.use('/categorie',CategorieRoute);
-app.listen(9000);
+app.use('/categorie', CategorieRoute);
+server.listen(9000);
